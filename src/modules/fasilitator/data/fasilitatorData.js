756a2bf738216ff997e20ce8a5b12773afeb6cc1
sofiaara1403 <sofiaara131@gmail.com>
2026-08-11 15:11:59 +0700

    feat(fasilitator): setup struktur modul fasilitator, training, cv

---
+++ src/modules/fasilitator/data/fasilitatorData.js
@@ -0,0 +1,20 @@
+export const fasilitatorList = [
+  {
+    id: "fas-001",
+    nama: "Contoh Nama",
+    gelar: null,
+    nik: null,
+    nip: null,
+    pangkat: null,
+    jabatan: null,
+    unitKerja: null,
+    alamat: null,
+    noHp: null,
+    email: null,
+    fotoUrl: null,
+    ttdUrl: null,
+    kompetensi: [],
+    riwayatPendidikan: [],
+    statusKelengkapan: "belum_lengkap", // 'lengkap' | 'belum_lengkap'
+  },
+];
